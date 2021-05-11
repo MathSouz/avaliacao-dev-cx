b11797a2bf2177c52977d3513c506566df99a49e
Matheus <mmedeiros.cbp@gmail.coom>
2021-05-11 17:44:28 -0300

    added root route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"))
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/view1.html");
+})
+
+
 app.listen(8080, () => {
     console.log("Servidor online");
 })
